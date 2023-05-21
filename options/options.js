321928452e8204ce0e7c08fa13c24ef18befa3bd
Crowdbotics <engineering@crowdbotics.com>
2023-05-21 23:13:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-night-dev-85152",
+  url: "https://curly-night-dev-85152.botics.co",
+  api: "https://curly-night-dev-85152.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
